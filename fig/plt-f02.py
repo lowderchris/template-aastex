6f969922af09d0e4ceb749e7cbae9ce059d65ce8
Chris Lowder <lowder.chris@icloud.com>
2016-11-24 13:08:09 +0000

    Let there be light.

---
+++ fig/plt-f02.py
@@ -0,0 +1,18 @@
+delta = 0.01
+x = np.arange(-1.5, 1.5, delta)
+y = np.arange(-1.5, 1.5, delta)
+X, Y = np.meshgrid(x, y)
+Z = cos(X**2 * 2*pi) * sin(Y**2 * 2*pi) * exp(-(X**2 + Y**2))
+
+f, ax1 = plt.subplots(1, figsize=[4, 2.472135955])
+#f, ax1 = plt.subplots(1)
+
+c1 = ax1.contour(X, Y, Z, levels=[-0.3, -0.2, -0.1, 0.1, 0.2, 0.3])
+
+ax1.set_xlabel('x-coordinate')
+ax1.set_ylabel('y-coordinate')
+
+ax1.minorticks_on()
+#grid(color='grey', zorder=0)
+
+savefig('f02.pdf')
